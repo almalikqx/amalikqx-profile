609b91044ec88e09504a5e8fb61c721778a2aef7
almalikqx <almalikqx.real@gmail.com>
2026-08-28 21:18:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
   name: "Malik",
-  username: "malik",
+  username: "amalikqx",
   bio: "Software Engineer & UI Designer. Suka ngopi dan ngoding tengah malam.",
   
-  profileUrl: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1760&auto=format&fit=crop",
+  profileUrl: "https://www.facebook.com/share/1EvKsLcF9v/",
   coverUrl: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=2564&auto=format&fit=crop",
   
   socials: {
-    youtube: "https://youtube.com/@malik",
-    facebook: "https://facebook.com/malik.123", // Bakal tetep nampil @malik
-    instagram: "https://instagram.com/malik",
-    threads: "https://threads.net/@malik",
-    x: "https://x.com/malik",
-    tiktok: "https://tiktok.com/@malik"
+    youtube: "https://youtube.com/@amalikqx",
+    facebook: "https://facebook.com/almalikqx", // Bakal tetep nampil @malik
+    instagram: "https://instagram.com/almalikqx",
+    threads: "https://threads.net/@almalikqx",
+    x: "https://x.com/amalikqx",
+    tiktok: "https://tiktok.com/@amalikqx"
   }
 };
