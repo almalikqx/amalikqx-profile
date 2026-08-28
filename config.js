d641e7bac324217547df76f2b5e86580c815fd14
almalikqx <almalikqx.real@gmail.com>
2026-08-28 21:37:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const CONFIG = {
   username: "amalikqx",
   bio: "Suka ngopi dan ngoding tengah malam.",
   
-  profileUrl: "https://www.facebook.com/share/1EvKsLcF9v/",
+  profileUrl: "https://www.facebook.com/photo.php?fbid=3679036175720708&set=a.1377865215837827",
   coverUrl: " https://i.pinimg.com/736x/0e/de/a0/0edea0f0a6da6f63e8dda679369d8398.jpg",
   
   socials: {
     youtube: "https://youtube.com/@amalikqx",
-    facebook: "https://facebook.com/almalikqx", // Bakal tetep nampil @malik
+    facebook: "https://facebook.com/almalikqx", // Bakal tetep nampil @amalikqx
     instagram: "https://instagram.com/almalikqx",
     threads: "https://threads.net/@almalikqx",
     x: "https://x.com/almalikqx",
