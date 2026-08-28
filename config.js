2511dd90fb370bb90bf585fe985d3601ffe6c738
almalikqx <almalikqx.real@gmail.com>
2026-08-28 21:03:06 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  name: "Malik",
+  username: "malik",
+  bio: "Software Engineer & UI Designer. Suka ngopi dan ngoding tengah malam.",
+  
+  profileUrl: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1760&auto=format&fit=crop",
+  coverUrl: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=2564&auto=format&fit=crop",
+  
+  socials: {
+    youtube: "https://youtube.com/@malik",
+    facebook: "https://facebook.com/malik.123", // Bakal tetep nampil @malik
+    instagram: "https://instagram.com/malik",
+    threads: "https://threads.net/@malik",
+    x: "https://x.com/malik",
+    tiktok: "https://tiktok.com/@malik"
+  }
+};
